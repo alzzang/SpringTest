f4ba11f79205edba18614bc6c2e42dd6c609d761
alzzang <sys5005@naver.com>
2016-06-09 14:08:38 +0900

    cal

---
+++ springtest/src/main/java/calculator/cal.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public interface cal {
+	public double plus(double num1, double num2);
+	public double minus(double num1, double num2);
+	public double mul(double num1, double num2);
+	public double div(double num1, double num2);
+	public double pow(double num1, double num2);
+	public double sqrt(double num1);
+}
