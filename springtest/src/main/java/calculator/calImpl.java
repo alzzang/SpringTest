d89ab9f7b598330f9eefb59eef36d67e15435286
jaewon <sffirefox@naver.com>
2016-06-09 14:13:11 +0900

    mul 만들었어요

--- springtest/src/main/java/calculator/calImpl.java
+++ springtest/src/main/java/calculator/calImpl.java
@@ -16,8 +16,7 @@ public class calImpl implements cal {
 
 	@Override
 	public double mul(double num1, double num2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return num1*num2;
 	}
 
 	@Override
