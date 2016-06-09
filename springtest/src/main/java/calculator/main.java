92131ddf9299db6a4dd83d6d83b1aed927460e7d
jaewon <sffirefox@naver.com>
2016-06-09 14:32:55 +0900

    a

--- springtest/src/main/java/calculator/main.java
+++ springtest/src/main/java/calculator/main.java
@@ -3,8 +3,8 @@ package calculator;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		cal cal = new calImpl();
+		System.out.println(cal.mul(2,3));
 	}
 
 }
