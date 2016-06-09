cb9cdaa867e873dc720503474a01fffd83c2904f
Lee Dong Woo <dlehddn630@naver.com>
2016-06-09 14:24:02 +0900

    ㅓㅕㅓㅏ

--- springtest/src/main/java/calculator/main.java
+++ springtest/src/main/java/calculator/main.java
@@ -5,18 +5,6 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("값: ");
-		double d = Double.parseDouble(sc.nextLine());
-		
-		calImpl c = new calImpl();
-		
-		System.out.println("결과: " + c.sqrt(d));
-=======
->>>>>>> branch 'master' of https://github.com/alzzang/SpringTest.git
 		
 	}
 
