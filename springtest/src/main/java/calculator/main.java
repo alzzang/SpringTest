a69da9333a8972c779bf3f076789001598318516
alzzang <sys5005@naver.com>
2016-06-09 14:41:54 +0900

    ㅇㅊㅊㅊ

--- springtest/src/main/java/calculator/main.java
+++ springtest/src/main/java/calculator/main.java
@@ -6,7 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
 		cal cal = new calImpl();
-		cal cal = new calImpl();
+		
 		System.out.println(cal.mul(2,3));
 		cal c= new calImpl();
 		System.out.println(c.mul(3, 2));
